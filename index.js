e2330c4fcaf873b9d8bad2e2db7ee26d3b85b1f9
Alex <pointalexandre@gmail.com>
2016-06-14 23:01:35 +0200

    Basic csv parser

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("running fine");
+import fs from 'fs';
+import csv from 'csv';
+import iconv from 'iconv-lite';
+
+var output = [];
+var parser = csv.parse({delimiter: '\t'})
+var input = fs.createReadStream('./data/CIS_CPD_bdpm.txt')
+	.pipe(iconv.decodeStream('win1252'))
+    .pipe(iconv.encodeStream('utf8'));
+
+var transformer = csv.transform(function(record, callback){
+  setTimeout(function(){
+    callback(null, record.join(' ')+'\n');
+  }, 500);
+}, {parallel: 10});
+input.pipe(parser).pipe(transformer).pipe(process.stdout);
