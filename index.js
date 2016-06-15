8502c1655ddc642ebe5c6ec57f9036e185ec4808
Alex <pointalexandre@gmail.com>
2016-06-15 23:58:35 +0200

    Parsing of generic drug groups

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 import fs from 'fs';
 import csv from 'csv';
 import iconv from 'iconv-lite';
+import _ from 'underscore';
 
 var output = [];
 var parser = csv.parse({delimiter: '\t'})
-var input = fs.createReadStream('./data/CIS_CPD_bdpm.txt')
-	.pipe(iconv.decodeStream('win1252'))
-    .pipe(iconv.encodeStream('utf8'));
 
-var transformer = csv.transform(function(record, callback){
-  setTimeout(function(){
-    callback(null, record.join(' ')+'\n');
-  }, 500);
-}, {parallel: 10});
-input.pipe(parser).pipe(transformer).pipe(process.stdout);
+var addEntry = function(record, output){
+	var groupId = record[0];
+	var medRef = {
+		cis: record[2],
+		type: record[3],
+		index: record[4]
+	};
+	var existingEntry = _.findWhere(output, {id: groupId});
+	if(existingEntry){
+		existingEntry.meds.push(medRef)
+	} else {
+		output.push({
+			id: groupId,
+			meds: [medRef]
+		})
+	}
+}
+
+//Use the writable stream api
+parser.on('readable', function(){
+  var record;
+  while(record = parser.read()){
+  	addEntry(record, output);
+    //output.push(record);
+  }
+});
+parser.on('finish', function(){
+	console.log("Done parsing " + output.length + " groups");
+})
+
+var input = fs.createReadStream('./data/CIS_GENER_bdpm.txt')
+	.pipe(iconv.decodeStream('win1252'))
+    .pipe(iconv.encodeStream('utf8'))
+    .pipe(parser);
+console.log("start parsing csv file")
+
+// var transformer = csv.transform(function(record, callback){
+//   setTimeout(function(){
+//     callback(null, record.join(' ')+'\n');
+//   }, 500);
+// }, {parallel: 10});
+//input.pipe(parser);
