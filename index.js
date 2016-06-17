78e4d4f194c2381ba6ba3ef13916b6d0fcdcefa9
Alex <pointalexandre@gmail.com>
2016-06-17 23:01:40 +0200

    Parser for list of drugs

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import {parseGenericGroups} from './src/drug-parser';
+import * as drugParser from './src/drug-parser';
 
-parseGenericGroups(function(drugGroups){
+drugParser.parseGenericGroups(function(drugGroups){
 	console.log(drugGroups.length + " groups parsed");
+	console.log(drugGroups[0])
+})
+
+drugParser.parseDrugs(function(drugs){
+	console.log(drugs.length + " drugs parsed");
+	console.log(drugs[0]);
 })
--- src/drug-parser.js
+++ src/drug-parser.js
@@ -3,7 +3,70 @@ import csv from 'csv';
 import iconv from 'iconv-lite';
 import _ from 'underscore';
 
-var addEntry = function(record, collection){
+var parserConfigs = [
+	{
+		name: 'generic groups',
+		fileName: 'CIS_GENER_bdpm.txt',
+		parseRow: function(row){
+			return {
+				id: row[0],
+				name: row[1],
+				cis: row[2],
+				type: row[3],
+				index: row[4]
+			}
+		}
+	},
+	{
+		name: 'drugs',
+		fileName: 'CIS_bdpm.txt',
+		parseRow: function(row){
+			return {
+				cis: row[0],
+				name: row[1],
+				form: row[2],
+				adminRoute: row[3],
+				authStatus: row[4],
+				authType: row[5],
+				marketState: row[6],
+				authDate: row[7],
+				status: row[8],
+				euroAuthNb: row[9],
+				owner: row[10],
+				enforcedMonitoring: row[11]
+			}
+		}
+	}
+]
+
+var parseFile = function(config, callback){
+	var records = [];
+
+	// setup parser
+	var parser = csv.parse({
+		delimiter: '\t',
+		relax: true
+	})
+	parser.on('readable', function(){
+	  var row;
+	  while(row = parser.read()){
+	  	records.push(config.parseRow(row));
+	  }
+	});
+	parser.on('finish', function(){
+		if(typeof(callback) === 'function'){
+			callback(records);
+		}
+	})
+
+	// Launch parsing
+	var input = fs.createReadStream('./data/' + config.fileName)
+		.pipe(iconv.decodeStream('win1252'))
+	    .pipe(iconv.encodeStream('utf8'))
+	    .pipe(parser);
+}
+
+var addGroup = function(record, collection){
 	var groupId = record[0];
 	var medRef = {
 		cis: record[2],
@@ -21,26 +84,12 @@ var addEntry = function(record, collection){
 	}
 }
 
+
+
 export function parseGenericGroups(callback){
-	var drugGroups = [];
+	parseFile(parserConfigs[0], callback);
+}
 
-	// setup parser
-	var parser = csv.parse({delimiter: '\t'})
-	parser.on('readable', function(){
-	  var record;
-	  while(record = parser.read()){
-	  	addEntry(record, drugGroups);
-	  }
-	});
-	parser.on('finish', function(){
-		if(typeof(callback) === 'function'){
-			callback(drugGroups);
-		}
-	})
-
-	// Launch parsing
-	var input = fs.createReadStream('./data/CIS_GENER_bdpm.txt')
-		.pipe(iconv.decodeStream('win1252'))
-	    .pipe(iconv.encodeStream('utf8'))
-	    .pipe(parser);
+export function parseDrugs(callback){
+	parseFile(parserConfigs[1], callback);
 }
